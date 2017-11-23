fbb2ca61229a7636683617d9ad1b39346377692a
Viet Duc Dao <daovi@cip.ifi.lmu.de>
2017-11-23 16:42:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main(){
         continue;
         }
 
-    if (connect(sockfd, &ai_addr, sizeof(ai_addrlen)) == -1) {
+    if (connect(sockfd, *ai_addr,ai_addrlen) == -1) {
         perror("connect");
         close(sockfd);
         return -1;
