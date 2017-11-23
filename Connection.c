c8ed3064df25f49827c635360c9af89b00e37ff3
Yunus Cobanoglu <YunusCobanoglu@Yunuss-MacBook-Air.local>
2017-11-23 14:30:58 +0100

    Main Funktion und ConnectionFunktion

---
+++ Connection.c
@@ -0,0 +1,156 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <netdb.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#define BUF 256
+#define Game-ID "NMMORRIS"
+#define PORTNUMBER 1357
+#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
+#define BUF_SIZE 256
+#define MES_LENGTH_SERVER 100
+#define ATTEMPTS_INVALID 20
+static char dataPRS[MES_LENGTH_SERVER];
+static char versionPRC []= "Version 2.0";
+static char game_idPRC []= "ID <<Game-ID>>";
+static char  numberOfPlayersPRC []= "Player";
+
+//ARGS: server message, if it begins with "+" return true
+bool static serverResponseValid(const char r[]){
+    if(strcmp(r[0], "+") == 0){
+        return true;
+    }
+    return false;
+}
+
+
+
+
+int static performConnection(int sockfd){
+    if(sockfd < 0){
+    perror("Invalid socket file descriptor");
+        close(sockfd);
+        return -1;
+    }else{
+        printf("Good to go?!");
+        }
+    //if testifvalid is negative then there was an error, repeat write() or read() if error
+ssize_t testifvalid = -1;
+//Number of invalid attempts you're allowed to have, amount -> see #define ATTEMPTS_INVALID, counts attempts up to this number
+int attempts = 0;
+
+//TODO: if Server doesn't respond with "+" -> error handling
+//S: <<Gameserver Version>>
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+        attempts++;
+            if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+                perror("Invalid server response");
+                return -1;
+            }
+
+        }
+        testifvalid = -1;
+
+//C: <<Client Version>>
+    while(testifvalid < 0){
+        testifvalid = write(sockfd, versionPRC, (int)strlen(versionPRC));
+            attempts++;
+            if(attempts >= ATTEMPTS_INVALID){
+                return -1;
+            }
+    }
+    testifvalid = -1;
+
+//S: Game-ID request
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+            if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+                perror("Invalid server respone");
+                return -1;
+        }
+    }
+    testifvalid = -1;
+
+    //C: <<Game-ID>>
+    while(testifvalid < 0){
+        testifvalid = write(sockfd, game_idPRC, (int)strlen(game_idPRC));
+        attempts++;
+        if(attempts >= ATTEMPTS_INVALID){
+            return -1;
+        }
+    }
+    testifvalid = -1;
+
+//S: <<Gamekind-Name>>
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+        if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+            perror("Invalid server respone");
+            return -1;
+        }
+    }
+    testifvalid = -1;
+
+        //S: <<Game-Name>>
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+        if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+            perror("Invalid server respone");
+            return -1;
+        }
+    }
+    testifvalid = -1;
+
+//C: [[Gewünschte Mitspielernummer]]
+    while(testifvalid < 0){
+        testifvalid = write(sockfd, numberOfPlayersPRC, (int)strlen(numberOfPlayersPRC));
+        attempts++;
+        if(attempts >= ATTEMPTS_INVALID){
+            return -1;
+        }
+    }
+    testifvalid = -1;
+
+//S: <<Mitspielernummer>> <<Mitspielername>>
+    while(testifvalid < 0){
+    testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+        if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+        perror("Invalid server respone");
+    return -1;
+    }
+    }
+    testifvalid = -1;
+
+//S: <<Mitspieleranzahl>>
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+        if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+        perror("Invalid server respone");
+        return -1;
+    }
+    }
+    testifvalid = -1;
+
+//S: <<Mitspielernummer>> <<Mitspielername>> <<Bereit>>
+    while(testifvalid < 0){
+        testifvalid = read(sockfd, dataPRS, MES_LENGTH_SERVER);
+
+        if(!serverResponseValid(dataPRS) || attempts >= ATTEMPTS_INVALID){
+            perror("Invalid server response");
+            return -1;
+        }
+    }
+    testifvalid = -1;
+    return 0;
+}
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,64 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <netdb.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#define BUF 256
+#define Game-ID "NMMORRIS"
+#define PORTNUMBER 1357
+#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
+#define BUF_SIZE 256
+#define MES_LENGTH_SERVER 100
+#define ATTEMPTS_INVALID 20
+
+
+
+
+int main(){
+
+    int sockfd;
+    int rv;
+
+    struct addrinfo hints, *servinfo, *p;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // use AF_INET6 to force IPv6
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo("sysprak.priv.lab.nm.ifi.lmu.de", "1357", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    exit(1);
+    }
+
+// loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+            perror("socket");
+        continue;
+        }
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        perror("connect");
+       close(sockfd);
+
+        }
+        fprintf(stderr,"connected\n");
+
+        break; // if we get here, we must have connected successfully
+    }
+
+    if (p == NULL) {
+    // looped off the end of the list with no connection
+    fprintf(stderr, "failed to connect\n");
+    exit(2);
+    }
+
+freeaddrinfo(servinfo); // all done with this structure
+ performConnection(sockfd);
+
+}
