53dceb6df510634669e0f6d29913b0645978b0c4
kolbeck <juri.kolbeck@hotmail.com>
2017-12-18 14:29:00 +0100

    added maintainconnection for second prolog

---
+++ maintainConnection.c
@@ -0,0 +1,26 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <netdb.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+short maintainConnection(int sockfd){
+  return 0;
+}
+
+short conWAIT(int sockfd){
+  return 0;
+}
+
+short conGAMEOVER(int sockfd){
+  return 0;
+}
+
+short conMOVE(in sockfd, char[]){
+  return 0;
+}
