d74ae7a6a1410b130d5c9d57c2e9024c1a33a372
kolbeck <juri.kolbeck@hotmail.com>
2017-12-29 23:24:46 +0100

    redundanzen entfernt

--- maintainConnection.c
+++ maintainConnection.c
@@ -29,7 +29,7 @@ int maintainConnectionFIRST(int sockfd, int firstServerCommand){
       perror("\nmaintainConnectionFIRST(): Error in performConnection\n");
       return ERROR;
     }
-	
+
 	 if(firstServerCommand == MOVE){
       printf("\nmaintainConnectionFIRST(): received MOVE from performConnection()\n");
 	  if(send_message(sockfd, MOVE) == ERROR){
@@ -37,7 +37,7 @@ int maintainConnectionFIRST(int sockfd, int firstServerCommand){
 		  return ERROR;
 	  }
     }
-	
+
 	if(firstServerCommand == WAIT){
       printf("\nmaintainConnectionFIRST(): received WAIT from performConnection()\n");
        if(send_message(sockfd, WAIT) == ERROR){
@@ -58,14 +58,14 @@ short maintainConnection(int sockfd){
 	  return -1;
    }
    printf("maintainConnection(): Server antwort:\"%s\"\n",serverResponse);
-   
+
     if(strstr(serverResponse,"+GAMEOVER")){
 		printf("\nmaintainConnection(): received +GAMEOVER from the server\n");
 		//TODO: React to GAMEOVER command
       free(serverResponse);
       return 0;
     }
-	
+
 	if(strstr(serverResponse,"+MOVE")){
 		printf("\nmaintainConnection(): received +MOVE from the server\n");
 		//TODO: React to MOVE command
@@ -73,7 +73,7 @@ short maintainConnection(int sockfd){
       free(serverResponse);
       return 0;
     }
-	
+
 	if(strstr(serverResponse,"+WAIT")){
 		printf("\nmaintainConnection(): received +WAIT from the server\n");
 		//TODO: React to WAIT command
@@ -92,7 +92,7 @@ int send_message(int sockfd, int type){
 		case MOVE: command = MOVE_MESSAGE; break;
 		case WAIT: command = WAIT_MESSAGE; break;
 	}
-	
+
 	if(write(sockfd, command, sizeof(command) < 0){
 		perror("\nsend_message(): write error");
 		return ERROR;
@@ -100,93 +100,23 @@ int send_message(int sockfd, int type){
 	return sockfd;
 }
 
-/*
-//ACHTUNG: MESSAGE VORHER IN messageToSend SCHREIBEN !
-int sendConMess(int sockfd, char* messageToSend){
-  const char* uselessChar;
-  uselessChar = "";
-  short attempts = 0;
-  short testifvalid = -1; //TODO deklaration dieser Variable verschieben ?
-  //char messageToSend = message;
-  if(strcmp(messageToSend, uselessChar) != 0){
-    //Siehe perfCon
-    while(testifvalid < 0){
-      testifvalid = write(sockfd, messageToSend, (int)strlen(messageToSend));
-      attempts++;
-      if(attempts >= ATTEMPTS_INVALID){
-          //messageToSend = "";
-          return -1;
-      }
-    }
-  }
-  else{
-    perror("messageToSend == 0, MAINCON");
-  }
-  printf("Wir senden: \"%s\" MAINCON\n", messageToSend);
-  //messageToSend = "";
-  return 0; //TODO wegtun
-}
-
-
-char* readConMess(int sockfd){
-  char *messageToRead;
-  char messageBuffer[MES_LENGTH_SERVER];
-
-  messageToRead=malloc(sizeof(messageToRead)); //messageToRead ???
-  short attempts = 0;
-  short testifvalid = -1;
-
-  while(testifvalid < 0){
-    testifvalid = read(sockfd, messageBuffer, MES_LENGTH_SERVER);
-    attempts++;
-    if(attempts >= ATTEMPTS_INVALID){
-      perror("Invalid server response");
-      printf("%sfehlertest",messageBuffer);
-      messageToRead =  "ERROR";
-      return messageToRead;
-    }
-  }
-  printf("Server sendet: \"%s\" MAINCON\n", messageBuffer);
-  strcpy(messageToRead,messageBuffer);
-  return messageToRead;
-  //TODO MEMORYLEAK VERHINDERN
-}
+//TODO readmessage ?
 
 short conWAIT(int sockfd){
-  sendConMess(sockfd, WAIT_MESSAGE);
-  return sockfd;
+  if((send_message(sockfd, WAIT))==0){
+		return 0;
+	}
+	else{
+		return -1;
+	}
 }
 
 
-short conGAMEOVER(int sockfd){
+short conGAMEOVER(int sockfd){ //TODO
   return sockfd;
 }
 
-short conMOVE(int sockfd){
+short conMOVE(int sockfd){ //TODO
   printf("yay move MAINCON");
-  //messageToSend = "OKWAIT\n";
-  char* messageCon = (char*)malloc(sizeof(char)*BUF);
-  messageCon = "OKWAIT\n";
-  if(sendConMess(sockfd, messageCon)<0){
-    perror("conMove fehler, MAINCON");
-  }
-  free(messageCon);
   return sockfd;
 }
-*/
-
-
-/*
-altes conwait
-char *clientResponse = (char*)malloc(sizeof(char)*BUF);
-//strcpy(clientResponse, ""); //DEFAULT WERT
-strcpy(clientResponse, "OKWAIT");
-strcat(clientResponse, "\n");
-
-int n = write(sockfd,clientResponse,sizeof(clientResponse));
-if(n<0){
-  perror("Fehler bei Schreiben +OKWAIT in Sockfd, maintainConnection.c");
-  return -1;
-}
-return 0;
-*/
