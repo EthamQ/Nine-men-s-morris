c0811d642c0284d103d3afb766b4665900710366
root <root@DESKTOP-8B0ISM1.localdomain>
2017-12-30 19:08:52 +0100

    Neuer header für Konstanten, jetzt alle in einem

---
+++ constants.h
@@ -0,0 +1,36 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+//Main server and game information
+#define GAMEKINDNAME "NMMORRIS"
+#define PORTNUMBER 1357
+#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
+#define CONFIG_DEFAULT "client.conf"
+
+
+
+//Flags
+#define ERROR -1
+#define MOVE 0
+#define WAIT 1
+#define GAMEOVER 2
+
+
+//Client messages
+#define MES_LENGTH_CLIENT 200
+#define WAIT_MESSAGE "OKWAIT\n"
+#define MOVE_MESSAGE "THINKING\n"
+
+//Server messages
+#define MES_LENGTH_SERVER 1048
+
+//Pipe
+#define PIPE_BUF 24
+
+
+//Other
+#define ATTEMPTS_INVALID 20
+#define BUFF_SIZE_SMALL 256
+
+
+#endif
