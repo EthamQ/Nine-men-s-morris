6853b442161060a60e2a5f3509a86290ba98595f
kolbeck <juri.kolbeck@hotmail.com>
2018-01-14 23:58:14 +0100

    test

--- readConfig.c
+++ readConfig.c
@@ -16,6 +16,7 @@ char* isPlayernumberAlreadyDefined = " ";
 int read_configfile(char* gameID, char* playerNumber, char* configFileName){
 FILE* file;
 char* filepath = configFileName;
+	printf("\nREADCONF: parameter: gameID : \"%s\" ,playernumber : \"%s\" , configFileName: \"%s\" \n", gameID,playerNumber, configFileName);
 
 	//Pruefen ob parameter gameID und playernumber valide sind
 	if(strcmp(gameID," ") != 0){
@@ -37,22 +38,31 @@ char* filepath = configFileName;
 			//printf("opened client.conf successfully, woohoo \n");
 		}
 	}
-	printf("test");
+	printf("test\n");
+
 	//in line werden die einzelnen Zeilen zwischengespeichert
 	char line[256];
 	char* ptr;
-	char delimiter[] = "=";
-	while (fgets(line, sizeof(line), file)){
-		ptr = strtok(line,delimiter);
+	char delimiter[] = "\n";
 
-		 while(ptr != NULL) {
-		 	printf("Ausgelesen:%s\n",ptr);
-		 	ptr = strtok(NULL,delimiter);
+	while (fgets(line, sizeof(line), file)){
+		configFileParameter = strtok(line,delimiter);
+	 	while(configFileParameter != NULL){
+
+			if( (strstr(configFileParameter, "§§$$")) != NULL || (strcmp(configFileParameter,"")==0) || (strcmp(configFileParameter," ")==0) ){
+				printf("Line wird ignoriert: \"%s\" \n",configFileParameter);
 			}
-	    }
-		printf("\n");
+			else{
+				printf("\nLine: \"%s\"\n",configFileParameter);
+				/*
+				if(assignParameters(configFileParameter) == -1){
+					perror("Error assigning Parameter from File to structure, READCONF");
+				}*/
+			}
+		 	configFileParameter = strtok(NULL,delimiter);
+		}
+  }
 
     fclose(file);
-
 return 0;
 }
