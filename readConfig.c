3f4a4b666289913c495a8c4bfaf78ab2984e1c49
kolbeck <juri.kolbeck@hotmail.com>
2018-01-14 23:50:28 +0100

    test

--- readConfig.c
+++ readConfig.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define paraHOSTNAME 1
+#define paraPORT 2
+#define paraGAMEKINDNAME 3
+#define paraGAMEID 4
+#define paraPLAYERNAME 5
+#define paraPLAYERNUMBER 6
+#define paraVERSION 7
+
+char* isGameidAlreadyDefined = " ";
+char* isPlayernumberAlreadyDefined = " ";
 
 int read_configfile(char* gameID, char* playerNumber, char* configFileName){
 FILE* file;
 char* filepath = configFileName;
 
-//Pruefen ob parameter gameID und playernumber valide sind
-if(strcmp(gameID," ") != 0){
-	isGameidAlreadyDefined = gameID;
-}
-if(strcmp(playerNumber," ") != 0){
-	isPlayernumberAlreadyDefined = playerNumber;
-}
+	//Pruefen ob parameter gameID und playernumber valide sind
+	if(strcmp(gameID," ") != 0){
+		isGameidAlreadyDefined = gameID;
+	}
+	if(strcmp(playerNumber," ") != 0){
+		isPlayernumberAlreadyDefined = playerNumber;
+	}
 //Datei oeffnen und ueberpruefen ob es die datei gibt, falls nicht wird client.conf eingelesen
-file = fopen(filepath, "r");
+	file = fopen(filepath, "r");
 	if(file == NULL){
 		printf("\ninvalid configfilename or path, opening client.conf instead\n\n");
 		fflush(file);
@@ -28,19 +38,19 @@ file = fopen(filepath, "r");
 			//printf("opened client.conf successfully, woohoo \n");
 		}
 	}
-//in line werden die einzelnen Zeilen zwischengespeichert
-char line[256];
-char* ptr;
-char delimiter[] = "=";
-while (fgets(line, sizeof(line), file)){
-	ptr = strtok(line,delimiter);
+	//in line werden die einzelnen Zeilen zwischengespeichert
+	char line[256];
+	char* ptr;
+	char delimiter[] = "=";
+	while (fgets(line, sizeof(line), file)){
+		ptr = strtok(line,delimiter);
 
-	 while(ptr != NULL) {
-	 	printf("Ausgelesen:%s\n",ptr);
-	 	ptr = strtok(NULL,delimiter);
-		}
-    }
-	printf("\n");
+		 while(ptr != NULL) {
+		 	printf("Ausgelesen:%s\n",ptr);
+		 	ptr = strtok(NULL,delimiter);
+			}
+	    }
+		printf("\n");
 
     fclose(file);
 
