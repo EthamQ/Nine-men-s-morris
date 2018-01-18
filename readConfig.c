5d3c66eed4ab10c32e9c22640e93044788b83a29
kolbeck <juri.kolbeck@hotmail.com>
2018-01-18 15:27:01 +0100

    \n wird jetzt korrekt hinzugetan

--- readConfig.c
+++ readConfig.c
@@ -141,7 +141,8 @@ short checkStructurComplete(){
 	if( (strcmp(confiConst.gameID, "") == 0) || (strcmp(confiConst.gameID, " ") == 0)  || (strcmp(confiConst.gameVersion, "ID \n") == 0) || (strcmp(confiConst.gameVersion, "ID  \n") == 0) ){
 		printf("GameID fehlt, bitte geben sie die GAMEID ein: ");
 		scanf("%s",scanVal);
-		strcpy(confiConst.gameID,scanVal);
+		strcpy(confiConst.gameID,"ID ");
+		strcat(confiConst.gameID,scanVal);
 		strcat(confiConst.gameVersion,"\n");
 		printf("Neue gameID: %s\n",confiConst.gameID);
 	}
