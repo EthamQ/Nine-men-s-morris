df48bd4e661eed8b5a046923001a4ca4cf40ac78
kolbeck <juri.kolbeck@hotmail.com>
2018-01-25 14:09:47 +0100

    draw.c: + durch@ersetzt

--- draw.c
+++ draw.c
@@ -10,7 +10,7 @@
 #define spalten 40
 
 void drawField(struct SHM_data* shm_pointer){
-   
+
     char field[zeilen][spalten];
     int i;
     int j;
@@ -122,20 +122,20 @@ void drawField(struct SHM_data* shm_pointer){
   field[14][38]='A';
   field[14][39]='4';
 
-  
+
 
       if(shm_pointer->field[0][0]==1){
         field[0][0]='#';
         field[0][1]='-';
       }if (shm_pointer->field[0][0]==2){
-        field[0][0]='+';
+        field[0][0]='@';
         field[0][1]='-';
       }
       if(shm_pointer->field[0][1]==1){
         field[0][19]='#';
         field[0][20]='-';
       }if (shm_pointer->field[0][1]==2){
-        field[0][19]='+';
+        field[0][19]='@';
         field[0][20]='-';
       }
       if(shm_pointer->field[0][2]==1){
@@ -143,55 +143,55 @@ void drawField(struct SHM_data* shm_pointer){
         field[0][39]='#';
       }if (shm_pointer->field[0][2]==2){
          field[0][38]='-';
-        field[0][39]='+';
+        field[0][39]='@';
       }
       if(shm_pointer->field[0][3]==1){
         field[7][38]='-';
         field[7][39]='#';
       }if (shm_pointer->field[0][3]==2){
         field[7][38]='-';
-        field[7][39]='+';
+        field[7][39]='@';
       }
       if(shm_pointer->field[0][4]==1){
         field[14][38]='-';
         field[14][39]='#';
       }if (shm_pointer->field[0][4]==2){
         field[14][38]='-';
-        field[14][39]='+';
+        field[14][39]='@';
 
       }
       if(shm_pointer->field[0][5]==1){
         field[14][19]='#';
         field[14][20]='-';
       }if (shm_pointer->field[0][5]==2){
-        field[14][19]='+';
+        field[14][19]='@';
         field[14][20]='-';
       }
       if(shm_pointer->field[0][6]==1){
         field[14][0]='#';
         field[14][1]='-';
       }if (shm_pointer->field[0][6]==2){
-        field[14][0]='+';
+        field[14][0]='@';
         field[14][1]='-';
       } if(shm_pointer->field[0][7]==1){
         field[7][0]='#';
         field[7][1]='-';
       }if (shm_pointer->field[0][7]==2){
-        field[7][0]='+';
+        field[7][0]='@';
         field[7][1]='-';
       }
       if(shm_pointer->field[1][0]==1){
         field[2][7]='#';
         field[2][8]='-';
       }if (shm_pointer->field[1][0]==2){
-        field[2][7]='+';
+        field[2][7]='@';
         field[2][8]='-';
       }
       if(shm_pointer->field[1][1]==1){
         field[2][19]='#';
         field[2][20]='-';
       }if (shm_pointer->field[1][1]==2){
-        field[2][19]='+';
+        field[2][19]='@';
         field[2][20]='-';
       }
       if(shm_pointer->field[1][2]==1){
@@ -199,53 +199,53 @@ void drawField(struct SHM_data* shm_pointer){
         field[2][31]='#';
       }if (shm_pointer->field[1][2]==2){
          field[2][30]='-';
-        field[2][31]='+';
+        field[2][31]='@';
       }if(shm_pointer->field[1][3]==1){
         field[7][30]='-';
         field[7][31]='#';
       }if (shm_pointer->field[1][3]==2){
         field[7][30]='-';
-        field[7][31]='+';
+        field[7][31]='@';
       }
       if(shm_pointer->field[1][4]==1){
         field[12][30]='-';
         field[12][31]='#';
       }if (shm_pointer->field[1][4]==2){
         field[12][30]='-';
-        field[12][31]='+';
+        field[12][31]='@';
       }if(shm_pointer->field[1][5]==1){
         field[12][19]='#';
         field[12][20]='-';
       }if (shm_pointer->field[1][5]==2){
-        field[12][19]='+';
+        field[12][19]='@';
         field[12][20]='-';
       }
       if(shm_pointer->field[1][6]==1){
         field[12][7]='#';
         field[12][8]='-';
       }if (shm_pointer->field[1][6]==2){
-        field[12][7]='+';
+        field[12][7]='@';
         field[12][8]='-';
       }
       if(shm_pointer->field[1][7]==1){
         field[7][7]='#';
         field[7][8]='-';
       }if (shm_pointer->field[1][7]==2){
-        field[7][7]='+';
+        field[7][7]='@';
         field[7][8]='-';
       }
       if(shm_pointer->field[2][0]==1){
         field[5][12]='#';
         field[5][13]='-';
       }if (shm_pointer->field[2][0]==2){
-        field[5][12]='+';
+        field[5][12]='@';
         field[5][13]='-';
       }
       if(shm_pointer->field[2][1]==1){
         field[5][19]='#';
         field[5][20]='-';
       }if (shm_pointer->field[2][1]==2){
-        field[5][19]='+';
+        field[5][19]='@';
         field[5][20]='-';
       }
       if(shm_pointer->field[2][2]==1){
@@ -253,34 +253,34 @@ void drawField(struct SHM_data* shm_pointer){
         field[5][26]='#';
       }if (shm_pointer->field[2][2]==2){
         field[5][25]='-';
-        field[5][26]='+';
+        field[5][26]='@';
       }
       if(shm_pointer->field[2][3]==1){
         field[7][25]='-';
         field[7][26]='#';
       }if (shm_pointer->field[2][3]==2){
         field[7][25]='-';
-        field[7][26]='+';
+        field[7][26]='@';
       }
       if(shm_pointer->field[2][4]==1){
         field[9][25]='#';
         field[9][26]='-';
       }if (shm_pointer->field[2][4]==2){
-        field[9][25]='+';
+        field[9][25]='@';
         field[9][26]='-';
       }
       if(shm_pointer->field[2][5]==1){
         field[9][19]='#';
         field[9][20]='-';
       }if (shm_pointer->field[2][5]==2){
-        field[9][19]='+';
+        field[9][19]='@';
         field[9][20]='-';
       }
       if(shm_pointer->field[2][6]==1){
         field[9][12]='#';
         field[9][13]='-';
       }if (shm_pointer->field[2][6]==2){
-        field[9][12]='+';
+        field[9][12]='@';
         field[9][13]='-';
       }
 
@@ -288,19 +288,9 @@ void drawField(struct SHM_data* shm_pointer){
         field[7][12]='#';
         field[7][13]='-';
       }if (shm_pointer->field[2][7]==2){
-        field[7][12]='+';
+        field[7][12]='@';
         field[7][13]='-';
       }
-
-
-
-
-
-
-
-
-
-
 
   for(i = 0; i<15; i++){
     for(j = 0; j<40; j++){
@@ -312,4 +302,3 @@ void drawField(struct SHM_data* shm_pointer){
   }
 printf("\n");
 }
-
